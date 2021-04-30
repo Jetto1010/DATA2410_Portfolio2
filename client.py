869c6502165fbd4335a04e02131ecb3dca339862
nikko <dordenik1@gmail.com>
2021-04-30 13:54:05 +0200

    checker problem

--- client.py
+++ client.py
@@ -6,6 +6,8 @@ WIN_SCALE = 8
 WIN = pygame.display.set_mode((WIDTH * WIN_SCALE, HEIGHT * WIN_SCALE))
 pygame.display.set_caption("PySnake")
 
+screen = pygame.Surface((WIDTH, HEIGHT))
+
 # Colors
 PEACH_ORANGE = (255, 201, 150)
 SALMON = (255, 132, 116)
@@ -20,9 +22,18 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+        
+        # Background
+        screen.fill(VICTORIA)
+        offset = 0
+        for x in range(WIDTH):
+            for y in range(HEIGHT):
+                if (x + offset)%2 == 0:
+                    screen.set_at((x, y), STRIKEMASTER)
 
-        WIN.fill(VICTORIA)
+        WIN.blit(pygame.transform.scale(screen, WIN.get_rect().size), (0, 0))
         pygame.display.update()
+
     pygame.quit()
 
 if __name__ == "__main__":
