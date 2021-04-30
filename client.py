4a00f09d1e02b8829c3887b66df65c6d5fd3b0ac
nikko <dordenik1@gmail.com>
2021-04-30 13:05:19 +0200

    Basic client function

--- client.py
+++ client.py
@@ -1,3 +1,29 @@
 import pygame
-WIDTH, HEIGHT = 900, 900
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+WIDTH, HEIGHT = 128, 75
+WIN_SCALE = 8
+
+WIN = pygame.display.set_mode((WIDTH * WIN_SCALE, HEIGHT * WIN_SCALE))
+pygame.display.set_caption("PySnake")
+
+# Colors
+PEACH_ORANGE = (255, 201, 150)
+SALMON = (255, 132, 116)
+VICTORIA = (88, 61, 114)
+STRIKEMASTER = (159, 95, 128)
+
+def main():
+
+    run = True
+    while run:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        WIN.fill(VICTORIA)
+        pygame.display.update()
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
