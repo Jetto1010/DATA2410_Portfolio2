7d13da47d1d4eed04bd83b766056b13f67b254da
nikko <dordenik1@gmail.com>
2021-05-02 21:58:49 +0200

    Random color, JSON on snake, Name input in menu

--- client.py
+++ client.py
@@ -18,18 +18,28 @@ pygame.display.set_caption("PySnake")
 screen = pygame.Surface((WIDTH, HEIGHT))
 
 # Colors
-PEACH_ORANGE = (255, 201, 150)
-SALMON = (255, 132, 116)
-VICTORIA = (26, 26, 26)
-TRENDY_PINK = (38, 38, 38)
+SNAKE_COLOR = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+TESTCOLOR_OTHERSNAKES = (255, 132, 116)
+BLACK = (26, 26, 26)
+GRAY = (38, 38, 38)
 
 # Snake attributes
+name = "Guest"
 velX, velY = 1, 0
 randX = random.randint(2, WIDTH - 10)
-randY = random.randint(2, HEIGHT - 2)
+randY = random.randint(2, HEIGHT - 4)
 snake_body = [(randX, randY + 3), (randX, randY + 2), (randX, randY + 1), (randX, randY)]
 posX, posY = snake_body[len(snake_body) - 1][0] + velX, snake_body[len(snake_body) - 1][1] + velY
 game_over = False
+
+snake = {
+    "name": name,
+    "position": snake_body,
+    "color": SNAKE_COLOR,
+    "game_over": game_over
+}
+
+print(snake)
 
 # Test data of fruit. This function is supposed to run on the server
 def create_fruit():
@@ -68,6 +78,7 @@ snakes = [
     }
 ]
 
+# Displays 5 highest scores ever gotten in the game
 highscores = [
     {
         "name": "nikko",
@@ -84,7 +95,7 @@ def draw_other_snakes():
     
     for snake in snakes:
         for pos in snake["position"]:
-            screen.set_at(pos, SALMON)
+            screen.set_at(pos, TESTCOLOR_OTHERSNAKES)
 
 # Draws the fruit coordinats from the server
 def draw_fruit():
@@ -150,14 +161,14 @@ def move_snake():
 def draw_snake():
     # Draws every "pixel" body of the snake
     for pos in snake_body:
-        screen.set_at(pos, PEACH_ORANGE)
+        screen.set_at(pos, SNAKE_COLOR)
 
 # Draws the background and assets onto the window
 def draw():
     global game_over
 
     # Drawing background
-    screen.fill(VICTORIA)
+    screen.fill(BLACK)
 
     for x in range(WIDTH):
         for y in range(HEIGHT):
@@ -166,7 +177,7 @@ def draw():
                 offset = 1
 
             if (x + offset) % 2 == 0:
-                screen.set_at((x, y), TRENDY_PINK)
+                screen.set_at((x, y), GRAY)
 
     # Drawing assets
     draw_fruit()
@@ -190,24 +201,40 @@ def show_menu():
     window.protocol("WM_DELETE_WINDOW", sys.exit)
 
     # Fonts
-    font = Font(family="Helvetica", size=14)
+    font = Font(family="Helvetica", size=12)
     titleFont = Font(family="Helvetica", size=24)
 
     # Title:
     title_label = tk.Label(text="PySnake", font=titleFont, padx=30, pady=20)
     title_label.pack()
 
+    # Name field:
+    name_label = Label(text="Name: ", font=font)
+    name_input = Entry(font=font)
+    name_label.pack()
+    name_input.pack()
+
     # Start game button:
-    startGame_button = Button(text="Start game", command=window.destroy,font=font , pady=5)
+    def start():
+        global name
+        name = name_input.get()
+        window.destroy()
+
+    startGame_button = Button(text="Start game", command=start ,font=font , pady=5)
     startGame_button.pack()
 
     # Highscores: 
-    score_text = "Highscore: \n"
+    score_text = "Highscores: \n"
     for score in highscores:
         score_text += "{}: {}\n".format(score["name"], score["score"])
 
-    score_label = tk.Label(text=score_text, font=font, padx=60, pady=30)
+    score_label = tk.Label(text=score_text, font=font, padx=60, pady=15)
     score_label.pack()
+
+    # Made by text:
+    made_text = "Made by:\n Nikola Dordevic, s348139\n Jørund Topp Løvlien, s341822"
+    made_label = tk.Label(text=made_text, padx=60, pady=10)
+    made_label.pack()
 
     window.mainloop()
 
@@ -220,26 +247,21 @@ def show_score():
     window.title("PySnake")
     window.resizable(0, 0)
 
-    font = Font(family="Helvetica", size=14)
+    font = Font(family="Helvetica", size=12)
 
     # Session score:
-    session_text = "Session score: \n"
+    session_text = "Session scores: \n"
     for snake in snakes:
        session_text += "{}: {}\n".format(snake["name"], len(snake["position"]) - 4)
-    session_label = tk.Label(text=session_text, font=font, padx=30, pady=20)
+    session_label = tk.Label(text=session_text, font=font, padx=30, pady=10)
     session_label.pack()
 
-    # Divider
-    divider_text = "==============="
-    divider_label = tk.Label(text=divider_text, padx=30, pady=20)
-    divider_label.pack()
-
     # Highscores:
-    score_text = "Highscore: \n"
+    score_text = "Highscores: \n"
     for score in highscores:
         score_text += "{}: {}\n".format(score["name"], score["score"])
 
-    score_label = tk.Label(text=score_text, font=font, padx=60, pady=10)
+    score_label = tk.Label(text=score_text, font=font, padx=60, pady=5)
     score_label.pack()
     
     window.mainloop()
@@ -248,6 +270,7 @@ def main():
     global run
 
     show_menu()
+    
 
     clock = pygame.time.Clock()
     threading.Thread(target=create_fruit).start()
@@ -261,6 +284,8 @@ def main():
 
     pygame.quit()
     show_score()
+
+
 
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -4,9 +4,3 @@ import data_pb2
 import data_pb2_grpc
 import time
 import threading
-
-def create_fruit():
-    return
-
-class Listener(data_pb2_grpc.DataServicer):
-    def sendPlayer
